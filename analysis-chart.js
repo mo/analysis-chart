f468ea67caacd023c1ea49f070fc4b7d505ab96a
Martin Olsson <martin@minimum.se>
2017-07-06 14:33:24 +0200

    Use default colors if series data has no colors

--- analysis-chart.js
+++ analysis-chart.js
@@ -88,7 +88,18 @@ class AnalysisChart {
       height: args.height || 500,
       interpolation: 'linear',
       stack: false,
-      series: args.series,
+      series: args.series.map((serie, idx) => {
+        if (!serie.color) {
+          const serieCopy = Object.assign({}, serie);
+          const DEFAULT_COLORS = [
+            '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f',
+            '#ffc670', '#17becf', '#76f992',
+          ];
+          serieCopy.color = DEFAULT_COLORS[idx % DEFAULT_COLORS.length];
+          return serieCopy;
+        }
+        return serie;
+      }),
     });
     graph.setRenderer('line');
     graph.render();
